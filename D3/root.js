395553997a824b66bb0496ff62940d35bd319da9
Hyejin Jenny Yeon <60158862+hjyeon@users.noreply.github.com>
2020-12-02 14:17:00 -0600

    still testing

--- D3/root.js
+++ D3/root.js
@@ -1,24 +1,25 @@
 let myPlot = document.getElementById('root'),
-    d3 = Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/coffee-flavors.csv',
-     function(err, rows){
+    d3 = Plotly.d3.csv('Musical Instruments2.csv',
+    function(err, rows){
     function unpack(rows, key) {
     return rows.map(function(row) {return row[key]})
     }
     var data = [{
-            type: "sunburst",
+            type: "treemap",
             maxdepth: 2,
-            ids: unpack(rows, 'ids'),
-            labels: unpack(rows, 'labels'),
-            parents: unpack(rows, 'parents'),
+            ids: unpack(rows, 'id'),
+            labels: unpack(rows, 'name'),
+            parents: unpack(rows, 'parent'),
             textposition: 'inside',
             insidetextorientation: 'radial'
     }]
+   
 
     var layout = {
         hovermode:'closest',
         title:'Click to see more'
     };
-    Plotly.newPlot('root', data, layout, {showSendToCloud: true})
+    Plotly.newPlot('root', data, layout, {showSendToCloud: false})
     myPlot.on('plotly_click', function(data){
         console.log(data.points[0].label);
         if(data.points[0].label == "Fruity") {            
