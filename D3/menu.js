1219c13057e35fcd95731524a36d0ee2a5970c74
Hyejin Jenny Yeon <60158862+hjyeon@users.noreply.github.com>
2020-12-14 19:02:38 -0600

    Fixed histogram bug

--- D3/menu.js
+++ D3/menu.js
@@ -1,10 +1,43 @@
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
-    document.getElementById("menu").style.width = "250px";
-  }
+  document.getElementById("menu").style.width = "250px";
+}
   
-  /* Set the width of the sidebar to 0 (hide it) */
-  function closeNav() {
-    document.getElementById("menu").style.width = "0";
-  }
-  
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("menu").style.width = "0";
+}
+
+function usefulLink(hyperlink) {
+  document.getElementById("detail").innerHTML = "";
+  document.getElementById("message").innerHTML = "";
+  document.getElementById("root").innerHTML = "<b> Here are some links to find more about Amazon categories: </b><br> <br>\
+  🔍 <a href=\"https://sellercentral.amazon.com/gp/help/external/G200332540?language=en_US\" target=\"_blank\">Amazon Overview of Categories</a><br>\
+  💸 <a href=\"https://sellercentral.amazon.com/gp/help/external/200336920\"target=\"_blank\">Amazon Category Fee Information</a><br>\
+  ❓ <a href=\"https://sellercentral.amazon.com/forums/t/why-has-amazon-locked-the-everything-else-category-any-one-know/405988\"target=\"_blank\">Why did Amazon disable Everything Else category?</a><br>"
+}
+
+function data() {
+  document.getElementById("detail").innerHTML = "";
+  document.getElementById("message").innerHTML = "";
+  document.getElementById("root").innerHTML = "💾 The complete dataset used for making this tool can be found \
+  <a href = \"https://www.kaggle.com/dataset/f17ddcc077d5868b9ce1ed8a3d267ad03ebf17d5f7a97fc1e12a3d16453499f2\"target=\"_blank\">HERE</a><br>\
+  📌 Metadata was downloaded from \
+  <a href = \"http://jmcauley.ucsd.edu/data/amazon/index.html\"target=\"_blank\">HERE</a><br>\
+  📌 Metadata was processed by modifying a python code written by \
+  <a href = \"https://pages.cs.wisc.edu/~gleicher/\"target=\"_blank\">Prof. Michael Gleicher</a><br>\
+  ⚠️ <a href=\"https://graphics.cs.wisc.edu/Courses/765-f2020/pages/dc3/\"target=\"_blank\">This tool was built as a project for the data visualization course.</a>\
+   The data used for making this tool is from 2014 data. Since the data is outdated, I do not recommend using this tool for getting real advice on selling on Amazon.<br>\
+  📑 <b>Table View for 85 Main Categories:</b> Average Price only considers products with known prices. If everything in the category has no price information, then the default value for the average price is $0. \
+  <iframe src=\"https://docs.google.com/spreadsheets/d/e/2PACX-1vQmyYsTOmHisR82yM_AHLmAjaI7d_TI9Vz20_tQdABgX-n60C95SUY_p1iB8CwPHcfO5sWYHu5HhrjL/pubhtml?widget=true&amp;headers=false\" width=\"900\" height=\"500\"></iframe>"
+}
+function hideMessage(button) {
+  let x = document.getElementById("message");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+      button.textContent = "Hide Message";
+    } else {
+      x.style.display = "none";
+      button.textContent = "Show Message";
+    }
+}
--- D3/root.js
+++ D3/root.js
@@ -40,11 +40,12 @@ function processMainData(err,rows) {
         type: "pie",
         values: unpack(rows,'subtreeProductCount'),
         labels: unpack(rows, 'name'),
+        ids: unpack(rows,'averagePrice'),
         textposition: 'inside',
         hole: 0.4,
         insidetextorientation: 'radial',
         text: unpack(rows,'fee'),
-        hovertemplate: '<b>%{label}</b><br>Number of Products: %{value}<br>Percent in Amazon: %{percent: .2%}<extra>Fee:<br>%{text}</extra>',
+        hovertemplate: '<b>%{label}</b><br>Number of Products: %{value}<br>Percent Share: %{percent: .2%}<extra>Fee:%{text}<br>Average Price: $%{id}</extra>',
         textinfo: 'percent',
         automargin: true
     }]
@@ -65,9 +66,9 @@ function processMainData(err,rows) {
     };    
     Plotly.newPlot('root', data, layout, config);
     mainMessage.innerHTML = "<b> &nbsp;&nbsp;Welcome to Amazon Seller Category Discovery Tool!</b> Amazon has over 20,000 categories, but your product can only go to <b>one</b> of these (and its upper-categories). This tool can help pick the right category for your product by providing a comparison between the categories. \
-    Amazon currently requires sellers to pick one category per product. Historically, un-categorized items were under \"Everything Else,\" but this is disabled as of 2018. Navigate to <b>&#9776; Menu >Useful Links</b> for more detailed information about this, category fees and more!  <br> \
-    &nbsp;&nbsp;The most important thing is to get the <b> main category </b> right. Often, a product fits multiple categories equally well. (As a formal seller), I suggest asking this question: \"Which category would my targeted buyers most likely to go into?\" <br><br> \
-    🖱️ Click the sector to see more price information. 🖱️ Click the category name in the legend to select/deselect the category. 🖱️🖱️ If you double click something in the legend, it will only select that. 📷 Click the Camera button shown as you mouse over to the chart to download the chart. 📑 You can navigate to <b>&#9776; Menu > About the Data</b> for the table view." 
+    Amazon currently requires sellers to pick one category per product. Historically, un-categorized items were under \"Everything Else,\" but this is disabled as of 2018. Navigate to <b>&#9776; Menu > Useful Links</b> for more detailed information about this, category fees and more!  <br> \
+    &nbsp;&nbsp;The most important thing is to get the <b> main category </b> right. Often, a product fits multiple categories equally well. (As a formal seller), I suggest asking this question: \"Which category would my targeted buyers most likely to go into?\" Use <b>Explore 85 Main Categories</b> above to see more details about each category. <br><br> \
+    🖱️ Click the sector to see the price information. 🖱️ Click the category name in the legend to select/deselect the category. 🖱️🖱️ If you double click something in the legend, it will only select that. 📷 Click the Camera button shown as you mouse over to the chart to download the chart. 📑 You can navigate to <b>&#9776; Menu > About the Data</b> for the table view." 
 
     myPlot.on('plotly_click', function(data){
         console.log(data.points[0].label);
@@ -145,7 +146,7 @@ function processData(err, rows) {
             insidetextorientation: 'radial',
             percentParent: 'percent parent',
             texttemplate:'<b>%{label}</b><br>Number of Products:<br>%{value}',
-            hovertemplate: '%{currentPath}<b>%{label}</b><br>Percentage of...<br> %{root}: %{percentRoot: .2%}<br> %{parent}: %{percentParent: .2%}<extra>Average Price: <br>$ %{text}</extra>',
+            hovertemplate: '%{currentPath}<b>%{label}</b><br>Number of Products: %{value}<br>Percentage of...<br> %{root}: %{percentRoot: .2%}<br> %{parent}: %{percentParent: .2%}<extra>Average Price: <br>$ %{text}</extra>',
     }]   
     let layout = {
         hovermode:'closest',
@@ -167,13 +168,13 @@ function processData(err, rows) {
      * Event Handler for clicking Treemap or Sunburst Chart
      */
     myPlot.on('plotly_click', function(data){
-        console.log(data.points[0].label);         
+        console.log(data.points[0].id);         
         let xs = ['Under $10','Under $25','Under $50','Under $100','Over $100','No price information'];
         let size = rows.length;
         let avgPrice = 0;
         let ys = [];
         for(i = 0 ; i < size ; i++) {
-            if (rows[i].name == data.points[0].label) {
+            if (rows[i].id == data.points[0].id) {
                 avgPrice = rows[i].averagePrice;
                 ys.push(rows[i].numUnder10);
                 ys.push(rows[i].numUnder25);
@@ -235,9 +236,10 @@ for(i =0 ; i < textByLine.length; i++) {
         for (i = 0 ; i < mainCategoriesCSV.length; i ++) {
             if (mainCategoriesCSV[i][0]==categoryName) {
                 mainMessage.innerHTML= "This is " + nameShown.bold() +" which contains "+ "<b>" + mainCategoriesCSV[i][3] + "</b>" +" main subcategories with <b>"+ mainCategoriesCSV[i][2] +"</b> products."+"\n"+
-                "The average known price of the products in this category is <b>$" +mainCategoriesCSV[i][4] + "</b>. Click to see more price information.\n"+
-                "The chart shows " + nameShown+ "'s the main subcategories. The relative size of boxes/sectors reflects the number of products in them."+"\n"+
-                "We recommend that you pick a subcategory if possible. Otherwise, your product will no longer show up once the buyer enters a subcategory of their interest."+"\n"
+                "The average known price of the products in this category is <b>$" +mainCategoriesCSV[i][4] + "</b>. 🖱️ Click a category for more price information.\n"+
+                "The chart below shows " + nameShown.bold()+ "'s subcategories. The relative size of boxes/sectors reflects the number of products in them."+"\n"+
+                "The white space reflects the number of products that are no further categorized. "+
+                "We recommend that you pick a subcategory if possible. <br> Otherwise, your product will no longer show up once the buyer enters a subcategory of their interest."+"\n"
             }
         }  
     }
@@ -274,37 +276,8 @@ function handleRadioClick(radio) {
     makeplot();
 }
 
-function usefulLink(hyperlink) {
-    document.getElementById("detail").innerHTML = "";
-    document.getElementById("message").innerHTML = "";
-    document.getElementById("root").innerHTML = "<b> Here are some links to find more about Amazon categories: </b><br> <br>\
-    🔍 <a href=\"https://sellercentral.amazon.com/gp/help/external/G200332540?language=en_US\" target=\"_blank\">Amazon Overview of Categories</a><br>\
-    💸 <a href=\"https://sellercentral.amazon.com/gp/help/external/200336920\"target=\"_blank\">Amazon Category Fee Information</a><br>\
-    ❓ <a href=\"https://sellercentral.amazon.com/forums/t/why-has-amazon-locked-the-everything-else-category-any-one-know/405988\"target=\"_blank\">Why did Amazon disable Everything Else category?</a><br>"
-}
 
-function data() {
-    document.getElementById("detail").innerHTML = "";
-    document.getElementById("message").innerHTML = "";
-    document.getElementById("root").innerHTML = "💾 The complete dataset used for making this tool can be found \
-    <a href = \"https://www.kaggle.com/dataset/f17ddcc077d5868b9ce1ed8a3d267ad03ebf17d5f7a97fc1e12a3d16453499f2\"target=\"_blank\">HERE</a><br>\
-    📌 Metadata was downloaded from \
-    <a href = \"http://jmcauley.ucsd.edu/data/amazon/index.html\"target=\"_blank\">HERE</a><br>\
-    📌 Metadata was processed by modifying a python code written by \
-    <a href = \"https://pages.cs.wisc.edu/~gleicher/\"target=\"_blank\">Prof. Michael Gleicher</a><br>\
-    📑 <b>Table View for 85 Main Categories:</b> Average Price only considers products with known prices. If everything in the category has no price information, then the default value for the average price is $0. \
-    <iframe src=\"https://docs.google.com/spreadsheets/d/e/2PACX-1vQmyYsTOmHisR82yM_AHLmAjaI7d_TI9Vz20_tQdABgX-n60C95SUY_p1iB8CwPHcfO5sWYHu5HhrjL/pubhtml?widget=true&amp;headers=false\" width=\"900\" height=\"500\"></iframe>"
-}
-function hideMessage(button) {
-    let x = document.getElementById("message");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-        button.textContent = "Hide Message";
-      } else {
-        x.style.display = "none";
-        button.textContent = "Show Message";
-      }
-}
+
 
 
 // Too lazy to read csv file... so here is the csv file as an array of arrays. 
